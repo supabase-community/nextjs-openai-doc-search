fcd2cfd228801ac47bf32c4d4188027dc4490848
Terry Sutton <saltcod@gmail.com>
2023-04-03 22:26:58 -0230

    Style cmdk popup

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,19 +1,18 @@
-import Head from "next/head";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import { SearchDialog } from "@/components/SearchDialog";
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import { SearchDialog } from '@/components/SearchDialog'
+import Image from 'next/image'
+import Link from 'next/link'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Next.js OpenAI Template</title>
-        <meta
-          name="description"
-          content="Next.js Template for building OpenAI applications with Supabase."
-        />
+        <meta name="description" content="Next.js Template for building OpenAI applications with Supabase." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -22,65 +21,28 @@ export default function Home() {
           <SearchDialog />
         </div>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <div className="py-8 w-full flex items-center justify-center space-x-6">
+          <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
+            <Link href="https://supabase.com" className="flex items-center justify-center">
+              <p className="text-base mr-2">Built by Supabase</p>
+              <Image src={'/supabase.svg'} width="20" height="20" alt="Supabase logo" />
+            </Link>
+          </div>
+          <div className="border-l border-gray-300 w-1 h-4" />
+          <div className="flex items-center justify-center space-x-4">
+            <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
+              <Link href="https://github.com/supabase/supabase" className="flex items-center justify-center">
+                <Image src={'/github.svg'} width="20" height="20" alt="Github logo" />
+              </Link>
+            </div>
+            <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
+              <Link href="https://twitter.com/supabase" className="flex items-center justify-center">
+                <Image src={'/twitter.svg'} width="20" height="20" alt="Twitter logo" />
+              </Link>
+            </div>
+          </div>
         </div>
       </main>
     </>
-  );
+  )
 }
